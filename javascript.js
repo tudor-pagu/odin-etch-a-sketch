ca579b74e49f5dbac5a37b1d5d41e640987be9b7
Tudor <pagu.tudor@gmail.com>
2022-04-30 19:01:49 +0300

    Make the colors random and darker on subsequent hovers

--- javascript.js
+++ javascript.js
@@ -5,6 +5,36 @@ const gridContainer = document.querySelector(".grid-container");
 ///returns undefined if it couldn't build grid for some reason, specifically if
 ///gridSize was too big. returns 1 if it builds grid succesfully
 ///builds grid in the gridContainer of gridSize * gridSize
+
+function getRandomTriple() {
+    return [ Math.floor(Math.random() * 255) , 
+            Math.floor(Math.random() * 255) , 
+            Math.floor(Math.random() * 255) ];
+}
+
+///takes a string of type "rgb(num,num,num)" and returns an array of the 3 numbers
+function parseRgb(rgb) {
+    return rgb.split(",").map(filterNumbers).map( (v) => parseInt(v) );
+}
+
+///takes a string and filters out all NaN characters, still a string afterwards
+
+function filterNumbers (str) {
+    return str.split("").filter(char => !isNaN(char)).join("");
+}
+
+function darkenSquareColor(event) {
+    let rgb = parseRgb(event.target.style.backgroundColor);
+    rgb = rgb.map((val) => Math.max(0 , val - 25));
+    let [r,g,b] = rgb;
+    event.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+function setSquareColor(event) {
+    let [r,g,b] = getRandomTriple();
+    event.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    event.target.addEventListener("mouseenter", darkenSquareColor);
+}
 function buildGrid(gridSize = 16) {
     if (gridSize >= 100) {
         return undefined;
@@ -20,9 +50,7 @@ function buildGrid(gridSize = 16) {
         for (let j = 0; j < gridSize; j++) {
             const square = document.createElement("div");
             square.classList.add("grid-square");
-            square.addEventListener("mouseenter", (event) => {
-                event.target.classList.add("activated");
-            });
+            square.addEventListener("mouseenter", setSquareColor , {once : true});
             row.appendChild(square);
         }
 
